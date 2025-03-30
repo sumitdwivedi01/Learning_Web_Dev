2c6afa83faa26d0aad97eab303f9c47260122086
sumitdwivedi01 <sumitdwivedi056@gmail.com>
2025-03-30 15:29:25 +0530

    fetch completed

--- fetch()/index.js
+++ fetch()/index.js
@@ -19,12 +19,30 @@
 //if the promise inside fetch is resolved then this will return a ok with a true value other wise ok will have false value
 //to catch the error we can use that like catching error with that
 fetchdata();
+function getRandomPokemon() {
+    const pokemonList = [
+        "Pikachu", "Charmander", "Bulbasaur", "Squirtle", "Jigglypuff", 
+        "Eevee", "Snorlax", "Gengar", "Mewtwo", "Lucario",
+        "Charizard", "Gyarados", "Dragonite", "Machamp", "Alakazam",
+        "Lapras", "Vaporeon", "Jolteon", "Flareon", "Umbreon",
+        "Espeon", "Sylveon", "Leafeon", "Glaceon", "Togepi",
+        "Tyranitar", "Metagross", "Salamence", "Rayquaza", "Infernape",
+        "Torterra", "Empoleon", "Darkrai", "Garchomp", "Zoroark",
+        "Chandelure", "Greninja", "Decidueye", "Incineroar", "Primarina",
+        "Zacian", "Zamazenta", "Eternatus", "Dragapult", "Cinderace"
+    ];
+    
+    const randomIndex = Math.floor(Math.random() * pokemonList.length);
+    return pokemonList[randomIndex];
+}
 
 async function fetchdata() {
     
     try {
-        const pkemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkemonName}`);
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        // const pokemonName = getRandomPokemon().toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         if(!response.ok){
             throw new Error("could not fetch resourse");
         }
